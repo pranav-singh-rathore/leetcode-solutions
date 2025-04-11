98094e0dfa9c6bd8e9562ccf0df5cf0864024622
Pranav Singh Rathore <pranavsinghrathore0507@gmail.com>
2025-04-11 19:08:54 +0200

    Optimised solution for 88. Merge Sorted Array

--- Easy/Array/88. Merge Sorted Array.java
+++ Easy/Array/88. Merge Sorted Array.java
@@ -1,35 +1,26 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        int res[]= new int[m+n];
-        while(i < m && j < n){
-            // int v1= nums1[i], v2= nums2[j];
-            if(nums1[i] <= nums2[j]){
-                res[k] = nums1[i];
-                // nums1[k] = nums1[i];
-                i++;
-                k++;
-            }else{
-                res[k] = nums2[j];
-                // nums1[k] = nums2[j];
-                j++;
-                k++;
+        int i = m - 1;        // Pointer at end of nums1's actual elements
+        int j = n - 1;        // Pointer at end of nums2
+        int k = m + n - 1;    // Pointer at end of nums1's total capacity
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
             }
+            k--;
         }
-       while(i < m){
-           res[k]= nums1[i];
-           i++;
-           k++;
-       }
-       while(j < n){
-           res[k]= nums2[j];
-           j++;
-           k++;
-       }
-        for(i = 0;i < nums1.length; i++){
-            nums1[i] = res[i];
+
+        // If nums2 still has remaining elements
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
+
     }
 }
